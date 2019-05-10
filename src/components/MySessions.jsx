3eb8471b92b4f7abacd3528b561f51b1bec85470
giorgoskoz <giorgoskoz@gmail.com>
2019-05-10 05:12:48 +0300

    working on my session, json received

--- src/components/MySessions.jsx
+++ src/components/MySessions.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import { Jumbotron, Container, Button, Row } from 'react-bootstrap';
+import axios from 'axios';
+import $ from 'jquery';
 
 class MySessions extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            sessionsList: []
+        };
+    }
+
+    componentDidMount(){
+            let that = this;
+            let gearArray = this.state.selectedExtraGear;
+            $.ajax({
+              url: "http://localhost:8080/sessions/for-user",
+              dataType: 'json',
+              contentType: "application/json; charset=utf-8",                       
+              type: 'get',         
+              headers: {'X-KLICKS-AUTH': this.context.token },
+              data: JSON.stringify(gearArray)
+          }).done(
+            function( response ) {
+              that.setState({sessionsList: response})
+            }).catch(
+                function( response ) {
+                    that.setState({sessionsList: response})
+                  })
+    }
 
     render(){
         return(
@@ -19,7 +46,9 @@ class MySessions extends React.Component {
                     <br></br>
                     <br></br>
                     </Container>
+
                 </Jumbotron>
+
             </div>
         )
     }
